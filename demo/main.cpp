295ea96b15fb1f3fc1976d8510e6766e98085836
Priyank Patel <tocubed@gmail.com>
2016-11-25 18:29:01 -0800

    Update demo to demonstrate transformations

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,6 +1,25 @@
-#include <glad/glad.h>
 #include <g4d/shader.hpp>
+#include <g4d/transform.hpp>
+
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/trigonometric.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+
+void print(const g4d::Transform& t)
+{
+	for (unsigned int i = 0; i < 4; i++)
+	{
+		for (unsigned int j = 0; j < 4; j++)
+			std::cout << (int)t.getLinearMap()[j][i] << " ";
+		std::cout << std::endl;
+	}
+	for (unsigned int i = 0; i < 4; i++)
+		std::cout << (int)t.getTranslation()[i] << " ";
+	std::cout << std::endl;
+}
 
 int main()
 {
@@ -17,12 +36,40 @@ int main()
 
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
 
-    while (!glfwWindowShouldClose(window))
+	g4d::Shader shader;
+	shader.loadFromFile(
+	    "../shaders/basic/vert.glsl", "../shaders/basic/geom.glsl",
+	    "../shaders/basic/frag.glsl");
+
+	g4d::Transform transform;
+	print(transform);
+	transform.rotate(
+	    glm::radians(90.0), glm::dvec4(0, 0, 1, 0), glm::dvec4(0, 0, 0, 1));
+	print(transform);
+	transform.rotate(
+	    glm::radians(90.0), glm::dvec4(0, 1, 0, 0), glm::dvec4(0, 0, 1, 0));
+	print(transform);
+	transform.rotate(
+	    glm::radians(90.0), glm::dvec4(1, 0, 0, 0), glm::dvec4(0, 1, 0, 0));
+	print(transform);
+	transform
+	    .scale(2, 2, 2, 2)
+		.translate(0, 0, 0, 2)
+	    .scale(0.5, 0.5, 0.5, 0.5)
+	    .translate(-1, -1, -1, 0);
+	print(transform);
+
+	glm::dvec4 result = transform.transform(glm::dvec4(1, 0, 0, 0));
+	for (unsigned int i = 0; i < 4; i++)
+		std::cout << (int)result[i] << " ";
+	std::cout << std::endl;
+
+	while (!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
 
         glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glfwSwapBuffers(window);
     }
